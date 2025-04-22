02379a4c3a15d578754bc33a432fcac3df846a39
dsb <dsbtek@gmail.com>
2025-04-23 00:14:21 +0100

    fixed gunicorn deployment error

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,10 @@
 from app import create_app
 
-app = create_app()
+# Rename 'app' to 'application' for Gunicorn
+application = create_app()
+
+# Keep 'app' for local development compatibility
+app = application
+
+if __name__ == '__main__':
+    app.run()
